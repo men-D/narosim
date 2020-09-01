58d9dd8d46784be7caae478cf3ffd979435f15a3
mendy <Mendi@mendi.local>
2020-09-01 12:38:47 -0400

    added redux

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createProject } from '../../store/action/projectActions';
 
 class CreateProject extends Component {
   state = {
@@ -6,34 +8,34 @@ class CreateProject extends Component {
     content: '',
   };
 
-  onChange = e => {
+  onChange = (e) => {
     this.setState({ [e.target.id]: e.target.value });
   };
 
-  onSubmit = e => {
+  onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.createProject(this.state);
   };
 
   render() {
     return (
-      <div className="container">
-        <form onSubmit={this.onSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Create New Project</h5>
-          <div className="Input-field">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" onChange={this.onChange} />
+      <div className='container'>
+        <form onSubmit={this.onSubmit} className='white'>
+          <h5 className='grey-text text-darken-3'>Create New Project</h5>
+          <div className='Input-field'>
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' onChange={this.onChange} />
           </div>
-          <div className="Input-field">
-            <label htmlFor="content">Project Content</label>
+          <div className='Input-field'>
+            <label htmlFor='content'>Project Content</label>
             <textarea
-              className="materialize-textarea"
-              id="cotent"
+              className='materialize-textarea'
+              id='cotent'
               onChange={this.onChange}
             />
           </div>
-          <div className="Input-field">
-            <button className="btn pink lighten-1 z-depth-0">Create</button>
+          <div className='Input-field'>
+            <button className='btn pink lighten-1 z-depth-0'>Create</button>
           </div>
         </form>
       </div>
@@ -41,4 +43,10 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(CreateProject);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
